13ee2f3b5d369c4848c3f90c1623b54d471b6fbe
pixelhubster <pixelhubster@gmail.com>
2024-10-04 11:03:33 +0000

    feat: create, edit, delete polygon

--- src/components/rightpanel.tsx
+++ src/components/rightpanel.tsx
@@ -1,32 +1,73 @@
 "use client"
-import React from 'react'
+import React, { useState } from 'react'
 import Mincard from './cards/mincard'
 import Summary from './cards/summary'
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { FeatureGroup, MapContainer, Polygon, TileLayer} from 'react-leaflet'
 import "leaflet/dist/leaflet.css"
+import L, { circle } from "leaflet"
+import { EditControl } from 'react-leaflet-draw'
+import { LatLngExpression } from 'leaflet'
+import "leaflet-draw/dist/leaflet.draw.css"
 
+const center: LatLngExpression = [51.515, -0.09]
+const purpleOptions = { color: 'green'}
+//  const polygon: LatLngExpression[] = [
+//    [51.515, -0.09],
+//    [51.52, -0.1],
+//    [51.52, -0.12],
+//  ]
 const Rightpanel = () => {
+   const [mapLayer, setMapLayer] = useState<any>([])
+   const onCreate = (e: any)=> {
+      console.log(e)
+      const {layerType, layer} = e;
+      if (layerType === "polygon") {
+         const {_leaflet_id} = layer;
+         setMapLayer( (layers: any) => [...layers, { id: _leaflet_id, latlngs: layer.getLatLngs()[0]}])
+      }
+
+   }
+   const onEdited = (e: any) => {
+      console.log(e);
+      const { layers: { _layers} } = e;
+
+      Object.values( _layers).map( ({ _leaflet_id, editing}: any) => {
+         setMapLayer( (layers: any) => layers.map((l: any) => l.id === _leaflet_id) && {...L, latLngs: { ...editing.latLngs[0]}})
+      })
+   }
+   const onDeleted = (e: any) => {
+
+      console.log(e);
+      const { layers: { _layers}} = e;
+      Object.values(_layers).map(( {_leaflet_id}: any) => {
+         setMapLayer( (layers: any) => layers.filter((l: any) => l.id !== _leaflet_id))
+      })
+   }
   return (
    <div className="w-full h-full bg-green-400 flex flex-col p-5">
-   <div className="w-full h-full bg-white rounded-xl border overflow-hidden" id="map">
-      <MapContainer className='w-full h-full' center={[48.2333, 2.4445]} zoom={13}>
+   <div className="w-full h-full bg-white rounded-xl border border-black overflow-hidden">
+      <MapContainer className='w-full' center={center} zoom={40}>
+         <FeatureGroup>
+            <EditControl position="topright" onCreated={onCreate} onEdited={onEdited} onDeleted={onDeleted}
+            draw={{
+               rectangle: false,
+               polyline: false,
+               circle: false,
+               circlemarker: false,
+               marker: false
+            }}
+             />
+         </FeatureGroup>
          <TileLayer
-            //  attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-         url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+             attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            url='https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.jpg'
+         // url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
          />
       </MapContainer>
+         {/* <Polygon pathOptions={purpleOptions} positions={polygon} /> */}
    </div>
+   
    <div className="w-full h-[18rem] bg-yellow-200 mt-5 p-2 flex gap-3 overflow-auto">
-      {/*
-         totalSupply: 10000,  // total crops grown in kg
-         estimatedRevenue: 25000,  // estimated revenue from total crops in USD
-         estimatedYieldPerAcre: 200,  // estimated yield per acre in kg
-         investmentPotential: {
-            estimatedROI: 15,  // estimated Return on Investment in percentage
-            riskLevel: "Moderate",  // risk level for investors
-            cropGrowthRate: 1.2  // rate of crop growth in percentage per season
-         }, 
-         */}
       <Summary header="Total Supply" data="23200300" className="bg-black shrink-0" />
       <Summary header="Estimated Revenue" data="23200300" className="bg-white text-black shrink-0" />
       <Summary header="Total Crops" data="23200300" className="bg-white text-black shrink-0" />
