0f0ef22ef5b2d83457e9a5ac6d5fe4c188570866
pixelhubster <pixelhubster@gmail.com>
2024-10-07 22:36:11 +0000

    eslint error

--- src/app/api/land/route.ts
+++ src/app/api/land/route.ts
@@ -1,18 +1,13 @@
 import { landContract } from "@/backend/web3";
 import { NextRequest, NextResponse } from "next/server"
-import Web3 from "web3";
-
-let landContractAddress = process.env.NEXT_PUBLIC_LANDCONTRACT;
 
 export async function GET(req: NextRequest) {
   try {
    const landDetails = await landContract.methods.getAllLandDetails().call()
    console.log(landDetails)
-   return NextResponse.json(landDetails)
-   
+   return NextResponse.json(landDetails, {status: 200})
   } catch (error) {
    console.log(error)
    return NextResponse.json({ error: "Couldn't fetch land details"}, { status: 500})
   }
 }
-
