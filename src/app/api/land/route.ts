cff504f27b19945e8a17dd7b763d2d6b057bc602
pixelhubster <pixelhubster@gmail.com>
2024-10-07 23:43:02 +0000

    nextjs error

--- src/app/api/land/route.ts
+++ src/app/api/land/route.ts
@@ -1,10 +1,13 @@
-import { landContract } from "@/backend/web3";
 import { NextRequest, NextResponse } from "next/server"
+import { Web3 } from 'web3'
+import abi from "../../../backend/contract/landToken.json"
 
 export async function GET(req: NextRequest) {
   try {
+   const provider = new Web3.providers.HttpProvider(process.env.NEXT_PUBLIC_BASE_SEPOLIA_RPC_URL as string);
+   const web3 = new Web3(provider);
+   const landContract = new web3.eth.Contract(abi.abi, "0x131e2C2Dfecae4d9eEFE60361797753d910CCD12")
    const landDetails = await landContract.methods.getAllLandDetails().call()
-   console.log(landDetails)
    return NextResponse.json(landDetails, {status: 200})
   } catch (error) {
    console.log(error)
