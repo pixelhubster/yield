b8be82508c6c1c57ffd74bd26f1fdc37fd2660f6
pixelhubster <pixelhubster@gmail.com>
2024-10-07 22:19:07 +0000

    fix: land route

--- src/app/api/land/route.ts
+++ src/app/api/land/route.ts
@@ -1,13 +1,14 @@
 import { landContract } from "@/backend/web3";
 import { NextRequest, NextResponse } from "next/server"
+import Web3 from "web3";
+
+let landContractAddress = process.env.NEXT_PUBLIC_LANDCONTRACT;
 
 export async function GET(req: NextRequest) {
   try {
-   const landDetails = await landContract.method.getAllLandDetails().call()
+   const landDetails = await landContract.methods.getAllLandDetails().call()
    console.log(landDetails)
-   return NextResponse.json({
-      data: landDetails
-   })
+   return NextResponse.json(landDetails)
    
   } catch (error) {
    console.log(error)
