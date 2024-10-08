6c64aaea8b565e33cf5354c57259b5111f025109
pixelhubster <pixelhubster@gmail.com>
2024-10-08 09:56:20 +0000

    fix: route response error

--- src/app/api/land/route.ts
+++ src/app/api/land/route.ts
@@ -4,8 +4,8 @@ import { landContract } from "@/backend/web3"
 export async function GET(req: NextRequest) {
   try {
    // const landDetails = await landContract.methods.getAllLandDetails().call()
-   // return NextResponse.json(landDetails, {status: 200})
    console.log("done")
+   return NextResponse.json({ success: "ok"}, {status: 200})
   } catch (error) {
    console.log(error)
    return NextResponse.json({ error: "Couldn't fetch land details"}, { status: 500})
