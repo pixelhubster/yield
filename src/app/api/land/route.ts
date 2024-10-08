a950839558e1806e993187feeddf01fc9d244e68
pixelhubster <pixelhubster@gmail.com>
2024-10-08 10:29:12 +0000

    fix: land route

--- src/app/api/land/route.ts
+++ src/app/api/land/route.ts
@@ -2,12 +2,11 @@ import { NextRequest, NextResponse } from "next/server"
 import { landContract } from "@/backend/web3"
 
 export async function GET(req: NextRequest) {
-  try {
-   // const landDetails = await landContract.methods.getAllLandDetails().call()
-   console.log("done")
-   return NextResponse.json({ success: "ok"}, {status: 200})
-  } catch (error) {
-   console.log(error)
-   return NextResponse.json({ error: "Couldn't fetch land details"}, { status: 500})
-  }
+   try {
+      const landDetails = await landContract.methods.getAllLandDetails().call()
+      return NextResponse.json(landDetails, { status: 200 })
+   } catch (error) {
+      console.log(error)
+      return NextResponse.json({ error: "Couldn't fetch land details" }, { status: 500 })
+   }
 }
