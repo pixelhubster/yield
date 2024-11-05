de4819b13810d901d5c3163e46543cf3456028f3
kaleel <pixelhubster@gmail.com>
2024-11-05 16:01:17 +0000

    fix: vercel env

--- frontend/src/app/api/register/route.ts
+++ frontend/src/app/api/register/route.ts
@@ -51,16 +51,16 @@ export async function GET(req: Request) {
    const id = searchParams.get("id")
    try {
       const contractResponse = await landContract.methods.ipfsHash().call()
-      console.log(contractResponse)
+      // console.log(contractResponse)
       const data = await pinata.gateways.get(cid || contractResponse);
-      console.log(data)
+      // console.log(data)
       // const data = await pinata.gateways.get("bafkreidmgywjwzkictk3t22iuqduc7cnmfkwqqjkc4g2ozvdzu4d3zghe4");
       // const blob = await (data as any).data.text().then((text: any) => {
       //    return JSON.parse(text)
       // })
       // const result = blob ?? data.data;
       const result = data.data as object;
-      console.log(result)
+      // console.log(result)
       if (id) {
          const tokenUri = await landContract.methods.tokenURI(id).call();
          const ipfsResponse = await pinata.gateways.get(tokenUri)
@@ -71,7 +71,7 @@ export async function GET(req: Request) {
          // })
          // const ifpsData = blob ?? data.data;
          const ifpsData = ipfsResponse.data;result
-         console.log(ifpsData)
+         // console.log(ifpsData)
          return NextResponse.json({id: id, tokenUri: tokenUri, ipfsdata: ifpsData, ...result}, { status: 200})
       }
       // if (result.data[0].weather.cod) return NextResponse.json({error: "Corrupted file"}, { status: 404}) 
